284fa53fcc3e9fbaa946c325e56ea54e25f9f6f1
Vaibhav Gupta <cs.utexas.edu>
2013-02-21 19:49:35 -0600

    some mroe header and footer changes

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
-    <div id="push"></div>
+    </div><!-- /container -->
+    </div><!-- /container -->
     </div><!-- /container -->
      <!-- Footer
       ================================================== -->
       
 
+ <div id="push"></div>
+</div><!-- /wrap -->
       <footer id="footer">
-		<div class="container">
-		<hr>
+    <div class="container">
+    <hr>
         <p class="pull-right"><a href="#">Back to top</a></p>
         <div class="links">
           <a href="http://www.vaibhav-gupta.com">Website</a>
@@ -14,9 +17,8 @@
           <a href="#">Sorry you can't Donate</a>
         </div>
         Made by <a target="_blank" href="#" onclick="pageTracker._link(this.href); return false;">Stephen Chiang & Vaibhav Gupta</a>. Sorry you can't contact us.<br/>
-	    </div><!-- /container -->
+      </div><!-- /container -->
       </footer>
-</div><!-- /wrap -->
 
 
 
@@ -26,23 +28,23 @@
     <script src="<?php echo base_url() ?>/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url() ?>/js/bootswatch.js"></script>
     <script>
-		$(document).ready(function() {
-			$('a[rel="tooltip"]').tooltip({trigger:'hover', placement:'top', html:'true'});
-			function updateTime(){
-				var currentTime = new Date()
-				var minutes = 60 - currentTime.getMinutes();
-				if (minutes < 10 && minutes > 1){
-					minutes = "<font class=\"error\">0" + minutes + " minutes</font>"
-				} else if ( minutes == 1 ) {
-					minutes = 60 - currentTime.getSeconds();
-					minutes = "<font class=\"error\">" + minutes + " seconds</font>"
-				} else {
-					minutes = "<font class=\"error\">" + minutes + " minutes</font>"
-				}
-				document.getElementById('time_span').innerHTML = minutes;
-			}
-			setInterval(updateTime, 1000);
-    	});
-	</script>
+    $(document).ready(function() {
+      $('a[rel="tooltip"]').tooltip({trigger:'hover', placement:'top', html:'true'});
+      function updateTime(){
+        var currentTime = new Date()
+        var minutes = 60 - currentTime.getMinutes();
+        if (minutes < 10 && minutes > 1){
+          minutes = "<font class=\"error\">0" + minutes + " minutes</font>"
+        } else if ( minutes == 1 ) {
+          minutes = 60 - currentTime.getSeconds();
+          minutes = "<font class=\"error\">" + minutes + " seconds</font>"
+        } else {
+          minutes = "<font class=\"error\">" + minutes + " minutes</font>"
+        }
+        document.getElementById('time_span').innerHTML = minutes;
+      }
+      setInterval(updateTime, 1000);
+      });
+  </script>
   </body>
 </html>
--- header.php
+++ header.php
@@ -71,31 +71,82 @@
 </head>
 <body onload="prettyPrint()">
 
-<div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="http://www.vaibhav-gupta.com">Vaibhav Gupta</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="<?= base_url() ?>">CodeNow</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->          
-          <div class="nav-collapse collapse pull-right">
-            <ul class="nav">
-              <li class="disabled"><a>Links will be lost in: <font id="time_span"><? $timer = 60-date('i'); if( $timer < 10) { echo "0".$timer; } else { echo $timer; } ?> minutes</font></a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-
-
 <div id="wrap">
+<div class="container">
+      <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+              <div class="container">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="brand" href="http://www.vaibhav-gupta.com">Vaibhav Gupta</a>
+                <div class="nav-collapse collapse">
+                  <ul class="nav">
+                    <li class="active"><a href="<?= base_url() ?>">CodeNow</a></li>
+                  </ul>
+                </div><!--/.nav-collapse -->          
+                <div class="nav-collapse collapse pull-right">
+                  <ul class="nav">
+                    <li class="disabled"><a>Links will be lost in: <font id="time_span"><? $timer = 60-date('i'); if( $timer < 10) { echo "0".$timer; } else { echo $timer; } ?> minutes</font></a></li>
+                  </ul>
+                </div><!--/.nav-collapse -->
+              </div>
+            </div>
+      </div>
+
+
 <div class="container">
 	<div class="container row-fluid staystrong">
 
+    </div><!-- /container -->
+    </div><!-- /container -->
+    </div><!-- /container -->
+     <!-- Footer
+      ================================================== -->
+      
+
+ <div id="push"></div>
+</div><!-- /wrap -->
+      <footer id="footer">
+    <div class="container">
+    <hr>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <div class="links">
+          <a href="http://www.vaibhav-gupta.com">Website</a>
+          <a href="http://www.github.com/kacxdak/sample">Github</a>
+          <a href="#">Sorry you can't Donate</a>
+        </div>
+        Made by <a target="_blank" href="#" onclick="pageTracker._link(this.href); return false;">Stephen Chiang & Vaibhav Gupta</a>. Sorry you can't contact us.<br/>
+      </div><!-- /container -->
+      </footer>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url() ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>/js/bootswatch.js"></script>
+    <script>
+    $(document).ready(function() {
+      $('a[rel="tooltip"]').tooltip({trigger:'hover', placement:'top', html:'true'});
+      function updateTime(){
+        var currentTime = new Date()
+        var minutes = 60 - currentTime.getMinutes();
+        if (minutes < 10 && minutes > 1){
+          minutes = "<font class=\"error\">0" + minutes + " minutes</font>"
+        } else if ( minutes == 1 ) {
+          minutes = 60 - currentTime.getSeconds();
+          minutes = "<font class=\"error\">" + minutes + " seconds</font>"
+        } else {
+          minutes = "<font class=\"error\">" + minutes + " minutes</font>"
+        }
+        document.getElementById('time_span').innerHTML = minutes;
+      }
+      setInterval(updateTime, 1000);
+      });
+  </script>
+  </body>
+</html>
